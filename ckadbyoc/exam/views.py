1ae254d5ac64fb24790df27705218f8a4e96bed5
Isidro <isidro.hernandez@sap.com>
2020-05-25 22:58:49 -0700

    installing front end this works

--- ckadbyoc/exam/views.py
+++ ckadbyoc/exam/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import Http404
+
+from .models import Questions
+
+
+def home(request):
+    questions = Questions.objects.all() 
+    return render(request, 'home.html', {
+        'questions': questions,
+    })
+
+
+def question_detail(request, question_id):
+    try:
+        question = Questions.objects.get(id=question_id)
+    except Questions.DoesNotExist:
+        raise Http404('question not found')
+    return render(request, 'question_detail.html', {
+        'question': question,
+    })
